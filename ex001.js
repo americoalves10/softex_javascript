fd73e4e5ab627736d954e22ea77134167361ddce
Americo Alves <americoalves10@gmail.com>
2025-08-14 14:56:54 -0300

    meu segundo commit

---
+++ ex001.js
@@ -0,0 +1,17 @@
+var n1 = 10
+var n2 = 8
+var n3 = 7
+var n4 = 9
+var soma = n1+n2+n3+n4
+var media = soma/4
+console.log(media);
+
+let a = 10
+let b = 20
+let c = 30
+
+let x1 = (-b + (Math.sqrt(b**2)-(4*a*c)))/(2*a)
+let x2 = (-b - (Math.sqrt(b**2)-(4*a*c)))/(2*a)
+
+console.log(x1)
+console.log(x2)
