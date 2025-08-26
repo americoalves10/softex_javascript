7f33ef7d5ae14e4bde2715d00f7256a013c4d877
americo alves <americoalves10@gmail.com>
2025-08-25 23:44:36 -0300

    exercicio 001  moodle

--- aula05/desafioex001.js
+++ aula05/desafioex001.js
@@ -1,20 +1,23 @@
+console.clear()
 let readline = require('readline-sync')
 
-let resposta = readline.question('  _______________|FORMULÁRIO DE CADASTRO|_______________?')
-console.log('|                                                         |')
-console.log('|                                                         |')
-console.log('|                                                         |')
-console.log('|                                                         |')
-console.log('|                                                         |')
-console.log('|                                                         |')
-console.log('|                                                         |')
-console.log('|                                                         |')
-console.log('|                                                         |')
-console.log('|_________________________________________________________|')
-let ano = 2025
-let idade = ano - resposta
-if (idade >= 18) {
-    console.log('PRESO')
-} else {
-    console.log('SOLTO')
-}
+let nome = readline.question('DIGITE SEU NOME COMPLETO? ')
+let dianasc = readline.question('DIGITE O DIA DE NASCIMENTO? ')
+let mesnasc = readline.question('DIGITE O MES DE NASCIMENTO? ')
+let anonasc = readline.question('DIGITE O ANO DE NASCIMENTO? ')
+let curso = readline.question('QUAL O CURSO QUE VOCE FAZ? ')
+let time = readline.question('QUAL O TIME QUE VOCE TORCE? ')
+
+console.clear()
+
+console.log('_________________________________________________________')
+console.log('|_______________| FORMULÁRIO DE CADASTRO |_______________|')
+console.log('|')
+console.log('| NOME: '+nome)
+console.log('|')
+console.log('| DIA: '+dianasc+'/'+mesnasc+'/'+anonasc)
+console.log('|')
+console.log('| CURSO: '+curso)
+console.log('|')
+console.log('| TIME: '+time)
+console.log('_________________________________________________________')
