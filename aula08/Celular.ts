0bd4099c2201cf4e8e25035d2f21b8de5c970047
vicente944-alt <contaptestes07@gmail.com>
2025-09-02 16:38:01 -0300

    aula 08 - poo

---
+++ aula08/Celular.ts
@@ -0,0 +1,80 @@
+class Cachorro {
+    public raça: String
+    public peso: String
+    public idade: Number
+    public cor: String
+    public altura: Number
+    public pedigree: String
+    dormir() {
+
+    }
+    comer() {
+        
+    }
+    correr() {
+
+    }
+    latir() {
+
+    }
+    acordar() {
+
+    }
+    treinar() {
+
+    }
+}
+class Celular {
+    public marca: String
+    public cor: String
+    public modelo: String
+    public camera: String
+    public flip: Boolean
+    public memoria: String
+    ligar() {
+
+    }
+    desligar() {
+        
+    }
+    tocar() {
+        
+    }
+    vibrar() {
+
+    }
+    armazenar() {
+
+    }
+    travar() {
+
+    }
+    
+}
+
+class Pessoa {
+    public nome: String
+    public peso: String
+    public idade: Number
+    public cor: String
+    public altura: Number
+    public cor_olho: String
+    dormir() {
+
+    }
+    comer() {
+        
+    }
+    correr() {
+
+    }
+    andar() {
+
+    }
+    acordar() {
+
+    }
+    treinar() {
+
+    }
+}
